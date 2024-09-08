a35861d35a9f2b41621b818cc1ce4c8bc168d531
rohildshah <shah5963@gmail.com>
2024-09-08 09:40:03 -0700

    Add basic FAQ component

---
+++ public/data/faq.ts
@@ -0,0 +1,15 @@
+type FAQ = {
+    question: string;
+    answer: string;
+}
+
+const faq: FAQ[] = [
+    {question: "What is ACM?",
+        answer: "ACM is ..."},
+    {question: "What are ACM branches?",
+        answer: "ACM branches are ..."},
+    {question: "How can I join?",
+        answer: "You can join by ..."}
+];
+
+export default faq;
