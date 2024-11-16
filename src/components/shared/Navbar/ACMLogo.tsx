334452c44840d986a56a09159b70dc7a24d36e25
Samuel Zhu <43155268+smmzhu@users.noreply.github.com>
2024-11-16 11:58:50 -0800

    quality of life — acm button reroutes to home (#53)

--- src/components/shared/Navbar/ACMLogo.tsx
+++ src/components/shared/Navbar/ACMLogo.tsx
@@ -1,22 +1,34 @@
 "use client";
-import { CardMedia, useMediaQuery } from "@mui/material";
+import { Button, CardMedia, useMediaQuery } from "@mui/material";
 
 const ACMLogo = () => {
   const isMobile = useMediaQuery("(max-width:600px)");
 
   return (
-    <CardMedia
-      component="img"
-      src={
-        isMobile ? "/assets/ACM logo.png" : "/assets/ACM logo with text 2.png"
-      }
-      alt="ACM Logo"
+    <Button
+      href="/"
+      disableRipple
       sx={{
-        height: "110px",
-        width: "auto",
-        objectFit: "contain",
+        transition: "transform 0.3s ease-in-out",
+        "&:hover": {
+          transform: "scale(1.05)",
+          backgroundColor: "#fff",
+        },
       }}
-    />
+    >
+      <CardMedia
+        component="img"
+        src={
+          isMobile ? "/assets/ACM logo.png" : "/assets/ACM logo with text 2.png"
+        }
+        alt="ACM Logo"
+        sx={{
+          height: "110px",
+          width: "auto",
+          objectFit: "contain",
+        }}
+      />
+    </Button>
   );
 };
 
